d44931d525a622d3441acb67797614ad57e63bdf
Matias Molina <molina.matias.87@gmail.com>
2022-05-03 00:37:07 -0300

    restaurado html

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
+app.set("view engine", "ejs");
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
@@ -15,6 +17,10 @@ app.get("/index.html", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
 
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
 app.get("/login", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
@@ -22,4 +28,3 @@ app.get("/login", (req, res) => {
 app.get("/producto", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/producto.html"));
 });
-
