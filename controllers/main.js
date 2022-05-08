6dfe285ec8c68ba336b9ca0879c0334b2622269a
mgecelewicz <mgecelewicz@gmail.com>
2022-05-07 21:14:14 -0300

    todos los cambios de Antonio Tonny del Sabado 7-5 hechos

--- controllers/main.js
+++ controllers/main.js
@@ -1,8 +1,59 @@
 const mainController = {
-  index: (req, res, next) => res.render("index"),
-  login: (req, res, next) => res.render("users/login"),
-  register: (req, res, next) => res.render("users/register"),
-  carrito: (req, res, next) => res.render("users/carrito"),
+
+  index: ( req, res, next ) => {
+
+      let productos = [
+
+          {
+              titulo: "Croquetas el perro feliz",
+              img_path: "/img/productos/alimento-ave-embuche.png",
+              descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, voluptatum accusantium fugit placeat obcaecati praesentium."
+          },
+          {
+              titulo: "Croquetas el gato feliz",
+              img_path: "/img/productos/alimento-ave-embuche.png",
+              descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, voluptatum accusantium fugit placeat obcaecati praesentium."
+          },
+          {
+              titulo: "Alimento el pez feliz",
+              img_path: "/img/productos/alimento-ave-embuche.png",
+              descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, voluptatum accusantium fugit placeat obcaecati praesentium."
+          },
+          {
+              titulo: "Alimento del ave feliz",
+              img_path: "/img/productos/alimento-ave-embuche.png",
+              descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, voluptatum accusantium fugit placeat obcaecati praesentium."
+          },
+          {
+              titulo: "Croquetas el perro feliz",
+              img_path: "/img/productos/alimento-ave-embuche.png",
+              descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, voluptatum accusantium fugit placeat obcaecati praesentium."
+          },
+          {
+              titulo: "Croquetas el gato feliz",
+              img_path: "/img/productos/alimento-ave-embuche.png",
+              descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, voluptatum accusantium fugit placeat obcaecati praesentium."
+          },
+          {
+              titulo: "Alimento el pez feliz",
+              img_path: "/img/productos/alimento-ave-embuche.png",
+              descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, voluptatum accusantium fugit placeat obcaecati praesentium."
+          },
+          {
+              titulo: "Alimento del ave feliz",
+              img_path: "/img/productos/alimento-ave-embuche.png",
+              descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, voluptatum accusantium fugit placeat obcaecati praesentium."
+          }
+
+      ];
+
+      res.render( "index", { productos: productos, titulo_pagina: "Petit and Fun" } );
+
+  },
+  login: ( req, res, next ) => res.render( "users/login", { titulo_pagina: "Petit and Fun - Login" } ),
+  register: ( req, res, next ) => res.render( "users/register", { titulo_pagina: "Petit and Fun - Registro" } ),
+  carrito: ( req, res, next ) => res.render( "users/carrito", { titulo_pagina: "Petit and Fun - Carrito" } ),
+
 };
 
 module.exports = mainController;
--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,10 @@
-const express = require("express");
-
-const router = express.Router();
+const express        = require("express");
+const router         = express.Router();
 const mainController = require("../controllers/main");
 
-router.get("/", mainController.index);
-router.get("/login", mainController.login);
-router.get("/register", mainController.register);
-router.get("/carrito", mainController.carrito);
+router.get( "/", mainController.index );
+router.get( "/login", mainController.login );
+router.get( "/register", mainController.register );
+router.get( "/carrito", mainController.carrito );
 
 module.exports = router;
