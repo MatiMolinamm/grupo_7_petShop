3e51d74cf22150379142bc0070bc5df8450f18f7
Matias Molina <molina.matias.87@gmail.com>
2022-05-09 23:44:23 -0300

    configure express.json en app.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 // Required modules
-const express         = require( "express" );
-const path            = require( "path" );
-const app             = express();
+const express = require("express");
+const path = require("path");
+const app = express();
 // Required routers
-const routerMain      = require( "./routes/main" );
-const routerProductos = require( "./routes/product" );
-const publicPath      = path.resolve( __dirname, "./public" );
+const routerMain = require("./routes/main");
+const routerProductos = require("./routes/product");
+const publicPath = path.resolve(__dirname, "./public");
 // Config
-app.set( "view engine", "ejs" );
-app.use( express.static( publicPath ));
+app.set("view engine", "ejs");
+app.use(express.static(publicPath));
+app.use(express.json());
 
 // Routes
-app.use( "/", routerMain );
-app.use( "/productos", routerProductos );
+app.use("/", routerMain);
+app.use("/productos", routerProductos);
 
-app.listen( 3000, () => console.log( "servidor funcionando en puerto 3000" ));
+app.listen(3000, () => console.log("servidor funcionando en puerto 3000"));
