2abacb6be6eec0e77114e28377bb72727e4175b6
mgecelewicz <mgecelewicz@gmail.com>
2022-05-13 23:37:33 -0300

    redisenio del carrito con estilos

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const routerMain = require("./routes/main");
 const routerProductos = require("./routes/product");
 const publicPath = path.resolve(__dirname, "./public");
 // Config
-app.set("view engine", "ejs");
-app.use(express.static(publicPath));
-app.use(express.json());
+app.set("view engine", "ejs"); // para definir ejs como el motor de vistas
+app.use(express.static(publicPath)); //para hacer publico los archivos 
+app.use(express.json()); //para que funcione el metodo post en formularios
 
 // Routes
 app.use("/", routerMain);
