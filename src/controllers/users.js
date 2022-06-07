3dbec09fedabe56da50f009cb7b2ea7dedf520d0
mgecelewicz <mgecelewicz@gmail.com>
2022-06-07 10:35:05 -0300

    validacion formulario REGISTER

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const bcryptjs = require("bcryptjs");
+const {validationResult} = require("express-validator");
 
 const usersController = {
   login: (req, res) =>
@@ -9,7 +10,18 @@ const usersController = {
     res.render("users/register", { titulo_pagina: "Petit and Fun - Registro" }),
   profile: (req, res) =>
     res.render("users/profile", { titulo_pagina: "Petit and Fun - Profile" }),
-  storeUsers: (req, res) => {
+  storeUsers: function ( req, res) {
+    console.log('Hola');
+    const resultValidation = validationResult(req);
+    
+    if (resultValidation.errors.length > 0) {
+      console.log(resultValidation);
+        return res.render( "users/register", { titulo_pagina: "Petit and Fun - Registro", 
+            errors: resultValidation.mapped(),
+            oldData: req.body
+        });
+    }  
+   
     const usersDataBaseFilePath = path.join(__dirname, "../data/users.json");
     const usersDataBase = JSON.parse(fs.readFileSync(usersDataBaseFilePath));
 
@@ -19,7 +31,7 @@ const usersController = {
 
     let userNew = {
       id: lastId + 1,
-      name: req.body.nombre,
+      name: req.body.name,
       telefono: req.body.telefono,
       email: req.body.email,
       categoria: req.body.categoria,
@@ -32,6 +44,7 @@ const usersController = {
     const usersDataBaseActualizadaJSON = JSON.stringify(usersDataBase);
     fs.writeFileSync(usersDataBaseFilePath, usersDataBaseActualizadaJSON);
     res.redirect("/");
+  
   },
 };
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const usersController = require("../controllers/users");
+const {body} = require('express-validator');
 
 //CONFIG
 const router = express.Router();
@@ -21,13 +22,39 @@ const storage = multer.diskStorage({
 
 const uploadFile = multer({ storage });
 
+//REGLAS DE VALIDACION
+const validations = [
+  body('name').notEmpty(). withMessage('Debe completar el campo')
+              .isAlpha(). withMessage('Solo puede utilizar letras')
+                .isLength({min: 3}). withMessage('Debe tener más de 3 caracteres'),
+  body('telefono').notEmpty(). withMessage('Debe completar el campo').bail()
+                .isNumeric(). withMessage('Debe ingresar números')
+                .isInt(). withMessage('Debe ingresar números enteros')
+                .isLength({min: 3}). withMessage('Debe tener mas de 3 caracteres'),
+  body('email').notEmpty(). withMessage('Debe completar el campo').bail(),
+  body('password').notEmpty(). withMessage('Debe completar el campo').bail()
+                .isLength({min: 3}). withMessage('Debe tener mas de 3 caracteres')
+                ,             
+  body('avatar').custom((value, { req }) => {
+  let file = req.file;
+  let acceptedExtensions = ['.jpg', '.png', '.gif'];
+  
+  if (!file) {
+    throw new Error('Tienes que subir una imagen');
+  } else {
+    let fileExtension = path.extname(file.originalname);
+    if (!acceptedExtensions.includes(fileExtension)) {
+      throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+    }
+  }
+
+  return true;
+})
+]
+
 //RUTAS REGISTRO DE USUARIOS
 router.get("/register", usersController.register);
-router.post(
-  "/register",
-  uploadFile.single("avatar"),
-  usersController.storeUsers
-);
+router.post("/register", uploadFile.single("avatar"), validations, usersController.storeUsers);
 
 // RUTAS LOGIN DE USUARIOS
 router.get("/login", usersController.login);
