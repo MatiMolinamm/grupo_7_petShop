b917b63a94e355bca93716561d0eab38cc9df5ea
Matias Molina <molina.matias.87@gmail.com>
2022-08-15 21:47:05 -0300

    actualizado

--- src/app.js
+++ src/app.js
@@ -28,6 +28,17 @@ app.use(
 );
 app.use(cookies());
 app.use(userLoggedMiddleware);
+// Middleware CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
 
 // Routes
 app.use("/", routerMain);
