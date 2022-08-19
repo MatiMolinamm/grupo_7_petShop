8f6085554bc06b8363fb88dc67ec1bc8f6381643
Luca Spiritini <luca.spiritini@hotmail.com>
2022-08-19 16:42:19 -0300

    refactor de header y css

--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,9 @@
 window.addEventListener("load", function (e) {
+
   let divResultado = document.querySelectorAll(".productos");
+
   let resultadoCero = document.querySelector(".resultadoCero");
+  
   let homeSearch = document.querySelector("#homesearch");
 
   homeSearch.addEventListener("keypress", function (e) {
@@ -12,4 +15,5 @@ window.addEventListener("load", function (e) {
     resultadoCero.innerHTML =
       "<h2>" + "No se encuentra el producto en la base de datos" + "</h2>";
   }
+
 });
